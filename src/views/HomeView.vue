dcdca3745255b83dc035ccf93c27cbffcf6b9605
Ygor Goulart <ygorgm95@gmail.com>
2023-02-10 16:47:14 -0300

    feat: add fetch function and basic render to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,35 @@
-<script></script>
-<template></template>
+<template>
+  <section>
+    <input type="text" v-model="pokemonId" />
+    <button @click="fetchPokemon">Fetch Pokemon</button>
+    <ul v-if="pokemonList.length">
+      <li v-for="pokemon in pokemonList" :key="pokemon.name">
+        {{ pokemon.name }}
+      </li>
+    </ul>
+  </section>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      pokemonId: "",
+      pokemonList: [],
+    };
+  },
+  methods: {
+    async fetchPokemon() {
+      const url = `https://pokeapi.co/api/v2/pokemon/${this.pokemonId}`;
+      try {
+        const response = await axios.get(url);
+        this.pokemonList = [response.data];
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
+</script>
 <style></style>
