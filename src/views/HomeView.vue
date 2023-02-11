d5935c20231ddc31cd0d8688a0a27642882ecba4
Ygor Goulart <ygorgm95@gmail.com>
2023-02-11 04:39:26 -0300

    feat: add Pinia MainStore to Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,58 @@
+<script setup>
+import { storeToRefs, mapActions } from "pinia";
+import Magnify from "vue-material-design-icons/Magnify.vue";
+import PokeCard from "@/components/PokeCard.vue";
+
+import { useMainStore } from "@/stores/main.js";
+const main = useMainStore();
+
+const { input, pokeList } = storeToRefs(main);
+const { fetchPokemon } = mapActions(useMainStore, ["fetchPokemon"]);
+</script>
+
 <template>
   <section>
-    <input type="text" v-model="pokemonId" />
-    <button @click="fetchPokemon">Fetch Pokemon</button>
-    <ul v-if="pokemonList.length">
-      <li v-for="pokemon in pokemonList" :key="pokemon.name">
-        {{ pokemon.name }}
-      </li>
+    <div class="input-container">
+      <input type="text" v-model="input" @keyup.enter="fetchPokemon(input)" />
+      <button @click="fetchPokemon(input)"><Magnify /></button>
+    </div>
+  </section>
+  <section>
+    <ul v-for="poke of pokeList">
+      <PokeCard :pokeName="poke" />
     </ul>
   </section>
 </template>
-<script>
-import axios from "axios";
 
-export default {
-  data() {
-    return {
-      pokemonId: "",
-      pokemonList: [],
-    };
-  },
-  methods: {
-    async fetchPokemon() {
-      const url = `https://pokeapi.co/api/v2/pokemon/${this.pokemonId}`;
-      try {
-        const response = await axios.get(url);
-        this.pokemonList = [response.data];
-      } catch (error) {
-        console.error(error);
-      }
-    },
-  },
-};
-</script>
-<style></style>
+<style lang="sass">
+@use '../assets/variables' as v
+
+section
+    display: flex
+    justify-content: center
+
+    padding: 1rem
+    color: v.$gray-8
+
+    input
+        padding: .5rem
+
+    .input-container
+        display: flex
+        border-radius: .5rem
+        background-color: v.$gray-1
+        outline: 1px solid v.$gray-8
+
+        button
+            cursor: pointer
+            display: flex
+            align-items: center
+            border-radius: .5rem
+            padding: 0 1rem
+            color: v.$gray-1
+            background: v.$gray-8
+
+            span
+                display: flex
+                align-items: center
+</style>
