f573d6d981a25c1dfaa03ac41592d12c4b932ad6
Ygor Goulart <ygorgm95@gmail.com>
2023-02-11 04:37:59 -0300

    refactor: main.js pinia

--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+const pinia = createPinia();
 const app = createApp(App);
 
-app.use(createPinia());
+app.use(pinia);
 app.use(router);
 
 app.mount("#app");
